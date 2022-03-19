16b539b7a15bbad4e6d9ab4c022ed9d0dc02aa66
Ania <owanky>
2022-03-19 12:17:31 +0100

    Detail

--- src/views/Detail.js
+++ src/views/Detail.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {Badge, Button, Card, Figure, ListGroup} from "react-bootstrap";
+import axios from "axios";
 
 const Detail = () => {
+    const [detail, setDetail] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await axios.get("/adverts/5");
+            //console.log(response.data);
+            setDetail(response.data);
+        }
+        fetchData();
+    }, []);
+
     return (
         <div>
-            hhhhhh
+            <div>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src={detail.image} />
+                    <Card.Body>
+                        <Card.Title>{detail.title}</Card.Title>
+                        <Card.Text>
+                            {detail.description}
+                        </Card.Text>
+                        <Button variant="primary">Kup</Button>
+                    </Card.Body>
+                </Card>
+            </div>
         </div>
     );
 };
