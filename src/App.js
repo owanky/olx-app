863bc81b70d2c40cd19cd2fa62dcda46169cef5a
Ania <owanky>
2022-03-19 12:08:08 +0100

    List

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
+import {Container, Image, Nav, Navbar} from "react-bootstrap";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import {LinkContainer} from "react-router-bootstrap";
+import List from "./views/List";
+import Detail from "./views/Detail";
 
-function App() {
+
+//raz wykonywane przy stracie
+
+function App() { //odswiezane co render
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <LinkContainer to="/">
+              <Navbar.Brand>
+                <Image src={logo} width={40} height={40}/>
+                Ania
+              </Navbar.Brand>
+            </LinkContainer>
+            <Nav className="me-auto">
+              <LinkContainer to="/list">
+                <Nav.Link>List</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/detail">
+                <Nav.Link>Detail</Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Container>
+        </Navbar>
+
+
+        <Container>
+          <Routes>
+            <Route path="/" element={<h1>Hello</h1>}/>
+            <Route path="/list" element={<List/>}/>
+            <Route path="/detail" element={<Detail/>}/>
+          </Routes>
+        </Container>
+      </BrowserRouter>
+
   );
 }
 
 export default App;
+
---
+++ src/views/List.js
@@ -0,0 +1,57 @@
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+import {Badge, Figure, ListGroup} from "react-bootstrap";
+
+const List = () => {
+    const [list, setList] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await axios.get("/adverts");
+            //console.log(response.data);
+            setList(response.data);
+        }
+        fetchData();
+    }, []);
+
+
+    return (
+        <div>
+            <ListGroup>
+
+
+                <ListGroup as="ul" >
+                    {list.map((advert) =>(
+                    <ListGroup.Item
+                        action key={advert.id}
+                        as="li"
+                        className="d-flex justify-content-between align-items-start"
+                    >
+                        <Figure>
+                            <Figure.Image
+                                width={171}
+                                height={180}
+                                alt="171x180"
+                                src={advert.image}
+                            />
+                        </Figure>
+                        <div className="ms-2 me-auto p-2">
+                            <div className="fw-bold">{advert.title}</div>
+                            <div>Seller: {advert.seller}</div>
+                            <div>Seller Phone: {advert.sellerPhone}</div>
+                        </div>
+
+                        <Badge bg="danger" pill>
+                            <h4>${advert.price}</h4>
+                        </Badge>
+
+                    </ListGroup.Item>
+                    ))}
+                </ListGroup>
+
+            </ListGroup>
+        </div>
+    );
+};
+
+export default List;
